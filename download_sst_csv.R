f81b51a087c3d3edb898674f2158979f2f1562c8
julietteverstaen <juliette.verstaen@noaa.gov>
2022-02-11 09:17:44 -1000

    comment out the SAI site code that only needs to be run once

--- download_sst_csv.R
+++ download_sst_csv.R
@@ -141,84 +141,86 @@ get_sst(status= "Inactive", region = "NWHI")
 get_sst(status= "Inactive", region = "PRIA")
 
 
+##### ----- #####
 
+# This is commented out now because I've already run this, but if it needs to be run again it can just be uncommented out
 
 # Grabbing the data associated with the duplicate site OCC-SAI-017 an Active CNMI site
 
 ## Need to grab the two coordinate rows associated with OCC-SAI-017. The following is just a repeat of the code in the loop above, but specifying the site, and lat/longs. The first set of coordinates associated with SAI-017 will be called lon_1/lat_1 and the second lon_2/lat_2.
 
 ### First one
-s <- "OCC-SAI-017"
-
-lon_1 <- as.numeric('145.7679')
-lat_1 <- as.numeric('15.25618')
-
-## url for the data
-url_link <- httr::GET(paste("https://oceanwatch.pifsc.noaa.gov/erddap/griddap/CRW_sst_v3_1.csv?analysed_sst%5B(1985-01-01T12:00:00Z):(2021-12-31T12:00:00Z)%5D%5B(", lat_1, ")%5D%5B(", lon_1, ")%5D&.draw=lines&.vars=time%7Canalysed_sst%7C&.color=0x000000&.bgColor=0xffffffff", sep = ""))
-
-## get csv data from the url
-bin <- content(url_link, "raw")
-writeBin(bin, "sst.csv")
-sst <- read_csv("sst.csv")
-sst <- sst[-1,] ## there's some extra unnecessary info in the first line under the header we can just get rid of
-sst <- sst %>%
-  mutate(OCC_SITEID = s)
-
-
-## save the file
-write_csv(sst, paste(gold_path, "SST-", s, "-1.csv", sep = ""))
-
-## Na counts check
-na_counts_file <- read_csv("occ_sites/sst_na_counts_location.csv") %>%
-  mutate(LATITUDE = as.numeric(LATITUDE),
-         LONGITUDE = as.numeric(LONGITUDE),
-         NA_COUNT = as.numeric(NA_COUNT))
-
-count_na <- sum(is.na(sst$analysed_sst))
-na_counts_file <- tibble::add_row(na_counts_file,
-                                  OCC_SITEID = s,
-                                  LATITUDE = lat_1,
-                                  LONGITUDE = lon_1,
-                                  NA_COUNT = count_na)
-
-## save it and overwrite old file
-write_csv(na_counts_file, "occ_sites/sst_na_counts_location.csv")
-
-
-### Second one
-s <- "OCC-SAI-017"
-
-lon_2 <- as.numeric('145.7676')
-lat_2 <- as.numeric('15.25614')
-
-## url for the data
-url_link <- httr::GET(paste("https://oceanwatch.pifsc.noaa.gov/erddap/griddap/CRW_sst_v3_1.csv?analysed_sst%5B(1985-01-01T12:00:00Z):(2021-12-31T12:00:00Z)%5D%5B(", lat_2, ")%5D%5B(", lon_2, ")%5D&.draw=lines&.vars=time%7Canalysed_sst%7C&.color=0x000000&.bgColor=0xffffffff", sep = ""))
-
-## get csv data from the url
-bin <- content(url_link, "raw")
-writeBin(bin, "sst.csv")
-sst <- read_csv("sst.csv")
-sst <- sst[-1,] ## there's some extra unnecessary info in the first line under the header we can just get rid of
-sst <- sst %>%
-  mutate(OCC_SITEID = s)
-
-
-## save the file
-write_csv(sst, paste(gold_path, "SST-", s, "-2.csv", sep = ""))
-
-## Na counts check
-na_counts_file <- read_csv("occ_sites/sst_na_counts_location.csv") %>%
-  mutate(LATITUDE = as.numeric(LATITUDE),
-         LONGITUDE = as.numeric(LONGITUDE),
-         NA_COUNT = as.numeric(NA_COUNT))
-
-count_na <- sum(is.na(sst$analysed_sst))
-na_counts_file <- tibble::add_row(na_counts_file,
-                                  OCC_SITEID = s,
-                                  LATITUDE = lat_2,
-                                  LONGITUDE = lon_2,
-                                  NA_COUNT = count_na)
-
-## save it and overwrite old file
-write_csv(na_counts_file, "occ_sites/sst_na_counts_location.csv")
-
+# s <- "OCC-SAI-017"
+#
+# lon_1 <- as.numeric('145.7679')
+# lat_1 <- as.numeric('15.25618')
+#
+# ## url for the data
+# url_link <- httr::GET(paste("https://oceanwatch.pifsc.noaa.gov/erddap/griddap/CRW_sst_v3_1.csv?analysed_sst%5B(1985-01-01T12:00:00Z):(2021-12-31T12:00:00Z)%5D%5B(", lat_1, ")%5D%5B(", lon_1, ")%5D&.draw=lines&.vars=time%7Canalysed_sst%7C&.color=0x000000&.bgColor=0xffffffff", sep = ""))
+#
+# ## get csv data from the url
+# bin <- content(url_link, "raw")
+# writeBin(bin, "sst.csv")
+# sst <- read_csv("sst.csv")
+# sst <- sst[-1,] ## there's some extra unnecessary info in the first line under the header we can just get rid of
+# sst <- sst %>%
+#   mutate(OCC_SITEID = s)
+#
+#
+# ## save the file
+# write_csv(sst, paste(gold_path, "SST-", s, "-1.csv", sep = ""))
+#
+# ## Na counts check
+# na_counts_file <- read_csv("occ_sites/sst_na_counts_location.csv") %>%
+#   mutate(LATITUDE = as.numeric(LATITUDE),
+#          LONGITUDE = as.numeric(LONGITUDE),
+#          NA_COUNT = as.numeric(NA_COUNT))
+#
+# count_na <- sum(is.na(sst$analysed_sst))
+# na_counts_file <- tibble::add_row(na_counts_file,
+#                                   OCC_SITEID = s,
+#                                   LATITUDE = lat_1,
+#                                   LONGITUDE = lon_1,
+#                                   NA_COUNT = count_na)
+#
+# ## save it and overwrite old file
+# write_csv(na_counts_file, "occ_sites/sst_na_counts_location.csv")
+#
+#
+# ### Second one
+# s <- "OCC-SAI-017"
+#
+# lon_2 <- as.numeric('145.7676')
+# lat_2 <- as.numeric('15.25614')
+#
+# ## url for the data
+# url_link <- httr::GET(paste("https://oceanwatch.pifsc.noaa.gov/erddap/griddap/CRW_sst_v3_1.csv?analysed_sst%5B(1985-01-01T12:00:00Z):(2021-12-31T12:00:00Z)%5D%5B(", lat_2, ")%5D%5B(", lon_2, ")%5D&.draw=lines&.vars=time%7Canalysed_sst%7C&.color=0x000000&.bgColor=0xffffffff", sep = ""))
+#
+# ## get csv data from the url
+# bin <- content(url_link, "raw")
+# writeBin(bin, "sst.csv")
+# sst <- read_csv("sst.csv")
+# sst <- sst[-1,] ## there's some extra unnecessary info in the first line under the header we can just get rid of
+# sst <- sst %>%
+#   mutate(OCC_SITEID = s)
+#
+#
+# ## save the file
+# write_csv(sst, paste(gold_path, "SST-", s, "-2.csv", sep = ""))
+#
+# ## Na counts check
+# na_counts_file <- read_csv("occ_sites/sst_na_counts_location.csv") %>%
+#   mutate(LATITUDE = as.numeric(LATITUDE),
+#          LONGITUDE = as.numeric(LONGITUDE),
+#          NA_COUNT = as.numeric(NA_COUNT))
+#
+# count_na <- sum(is.na(sst$analysed_sst))
+# na_counts_file <- tibble::add_row(na_counts_file,
+#                                   OCC_SITEID = s,
+#                                   LATITUDE = lat_2,
+#                                   LONGITUDE = lon_2,
+#                                   NA_COUNT = count_na)
+#
+# ## save it and overwrite old file
+# write_csv(na_counts_file, "occ_sites/sst_na_counts_location.csv")
+#
